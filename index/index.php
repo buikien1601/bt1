c4b13c12563da79f9798e2f0ea465472986400b1
Bui Trung Kien <buikien16012000@gmail.com>
2021-09-19 21:22:05 +0700

    Bui Trung Kien bai tap 1

---
+++ index/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- --- -->
+    <section class="navbar">
+        
+        <div class="container">
+            <div class="logo">
+                <img src="../anh/logo.png" alt="Restaurant Logo" class="img-responsive">
+            </div>
+        <div class="menu text-right">
+            <ul>
+                
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Categories</a>
+                </li>
+                <li>
+                    <a href="#">Foods</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+       
+       <div class="clearfix">
+       
+       </div>
+       
+    </section>
+    <!-- --- -->
+
+    <!-- --- -->
+    <section class="food-search text-center">
+        
+        <div class="container">
+          <form action="">
+              <input type="search" name="search" placeholder="Search for Food..">
+              <input type="submit" name="submit" value="Search" class="btn btn-primary">
+          </form>
+        </div>
+        
+      
+    </section>
+    <!-- --- -->
+       
+    <!-- --- -->
+    <section class="categories">
+        
+        <div class="container">
+        <h2 class="text-center">Explore Foods</h2>
+        <a href="">
+           <div class="box-3 float-container">
+             <img src="../anh/pizza.jpg" alt="Pizza" class=" img-responsive img-curve">
+             <h3 class="float-text text-white" > Pizza</h3>
+           </div>
+        </a>
+        <a href="">
+           <div class="box-3 float-container">
+            <img src="../anh/burger.jpg" alt="Burger" class=" img-responsive img-curve">
+            <h3 class="float-text text-white" > Bruger</h3>
+           </div>
+        </a>
+        <a href="">
+           <div class="box-3 float-container">
+            <img src="../anh/momo.jpg" alt="Momo" class=" img-responsive img-curve">
+            <h3 class="float-text text-white" > Momo</h3>
+           </div>
+        </a>
+          <div class="clearfix"></div>
+        </div>
+      
+     
+    </section>
+    <!-- --- -->
+
+    <!-- --- -->
+    <section class="food-menu">
+        
+        <div class="container">
+         <h2 class="text-center">Food Mennu</h2>
+         <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="../anh/menu-pizza.jpg" alt="Chicke Hawain Pizza" class=" img-responsive  img-curve">
+            </div>
+            <div class="food-menu-desc">
+                <h4>Food Title </h4>
+                <p class="food-price">$2,3</p>
+                <p class="food-detail">
+                    Made with Italian Sauce,Chicken,anh organice vegetables.
+                </p>
+                <br>
+                <a href="#" class="btn btn-primary"> Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="../anh/menu-burger.jpg" alt="Chicke Hawain Burger" class=" img-responsive  img-curve">
+            </div>
+            <div class="food-menu-desc">
+                <h4>Smoky Burger </h4>
+                <p class="food-price">$2,3</p>
+                <p class="food-detail">
+                    Made with Italian Sauce,Chicken,anh organice vegetables.
+                </p>
+                <br>
+                <a href="#" class="btn btn-primary"> Order Now</a>
+            </div>
+        </div>
+
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="../anh/menu-momo.jpg" alt="Chicke Hawain Pizza" class=" img-responsive  img-curve">
+            </div>
+            <div class="food-menu-desc">
+                <h4>Chiken Steam Momo </h4>
+                <p class="food-price">$2,3</p>
+                <p class="food-detail">
+                    Made with Italian Sauce,Chicken,anh organice vegetables.
+                </p>
+                <br>
+                <a href="#" class="btn btn-primary"> Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="../anh/menu-pizza.jpg" alt="Chicke Hawain Pizza" class=" img-responsive  img-curve">
+            </div>
+            <div class="food-menu-desc">
+                <h4>Food Title </h4>
+                <p class="food-price">$2,3</p>
+                <p class="food-detail">
+                    Made with Italian Sauce,Chicken,anh organice vegetables.
+                </p>
+                <br>
+                <a href="#" class="btn btn-primary"> Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="../anh/menu-pizza.jpg" alt="Chicke Hawain Pizza" class=" img-responsive  img-curve">
+            </div>
+            <div class="food-menu-desc">
+                <h4>Food Title </h4>
+                <p class="food-price">$2,3</p>
+                <p class="food-detail">
+                    Made with Italian Sauce,Chicken,anh organice vegetables.
+                </p>
+                <br>
+                <a href="#" class="btn btn-primary"> Order Now</a>
+            </div>
+        </div>
+        <div class="food-menu-box">
+            <div class="food-menu-img">
+                <img src="../anh/menu-pizza.jpg" alt="Chicke Hawain Pizza" class=" img-responsive  img-curve">
+            </div>
+            <div class="food-menu-desc">
+                <h4>Food Title </h4>
+                <p class="food-price">$2,3</p>
+                <p class="food-detail">
+                    Made with Italian Sauce,Chicken,anh organice vegetables.
+                </p>
+                <br>
+                <a href="#" class="btn btn-primary"> Order Now</a>
+            </div>
+        </div>
+            
+            
+            <div class="clearfix"></div>
+        
+       
+        
+       
+    </section>
+    <!-- --- -->
+    <section class="social">
+        
+        <div class="container text-center">
+              <ul>
+                  <li href="#">
+                      <a href="#"><img src="https://img.icons8.com/fluency/48/000000/facebook-circled.png"/></a>
+                  </li>
+                  <li href="#">
+                     <a href="#"><img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/></a>
+                  </li>
+                  <li href="#">
+                     <a href="#"> <img src="https://img.icons8.com/fluency/48/000000/twitter.png"/></a>
+                  </li>
+              </ul>
+        </div>
+        
+      
+    </section>
+    <!-- --- -->
+
+    <!-- --- -->
+    <section class="footer">
+        
+        <div class="container text-center">
+              <p>All rights reserved. Designed By <a href="#">CSE.TLU</a></p>
+        </div>
+        
+      
+    </section>
+    <!-- --- -->
+</body>
+</html>
