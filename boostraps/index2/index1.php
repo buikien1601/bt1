910530effc0a77eae1102852b3e3be9f5751d247
buikien1601 <66303699+buikien1601@users.noreply.github.com>
2021-10-28 20:42:35 +0700

    Add files via upload

---
+++ boostraps/index2/index1.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css.css">
+  <link href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="col-sm-1"></div>
+    <a class="navbar-brand" href="#"><img src="../anh/logo.png" alt=""></a>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="col-sm-8"></div>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item ">
+          <a class="nav-link" href="#" style="color: #ff6b81; font-weight: 500">Home <span
+              class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: #ff6b81 ; font-weight: 500">Categories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: #ff6b81; font-weight: 500">Foods</a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link " href="#" style="color: #ff6b81;font-weight: 500">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <section class="food-search text-center">
+        
+        <div class="container">
+          <form action="">
+              <input type="search" name="search" placeholder="Search for Food..">
+              <input type="submit" name="submit" value="Search" class="btn btn-primary"
+              style="background-color: #ff6b81;">
+          </form>
+        </div>
+
+
+  </section> 
+
+
+  <div class="container-fluid">
+    <h1 class="text-center mt-5 mb-5">Explore Foods</h1>
+    ​ <div class="d-flex justify-content-around">
+
+      <div class="d-flex align-items-end justify-content-center" style="width: 450px; height: 500px; background-image: url(./anh/burger.jpg); border-radius: 10px;">
+        <h2 style="z-index:9; color: white">Burger</h2>
+      </div>
+
+      <div class="d-flex align-items-end justify-content-center" style="width: 450px; height: 500px; background-image: url(./anh/pizza.jpg); border-radius: 10px;">
+        <h2 style="z-index:9; color: white">Pizza</h2>
+      </div>
+
+      <div class="d-flex align-items-end justify-content-center" style="width: 450px; height: 500px; background-image: url(./anh/momo.jpg); border-radius: 10px;">
+        <h2 style="z-index:9; color: white">MoMo</h2>
+      </div>
+      
+    </div>
+  
+ 
+
+    <div class="container-fuild pt-5" style="margin-top: 80px; background-color: #ececec;">
+    <h2 class="text-center">Food Menu</h2>
+
+    <div class="d-flex justify-content-around" style="width: 80%; margin: 50px auto;">
+      
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./img/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">Food Title</h4>
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">$2,3</h4>
+            <p style="font-size: 1rem;
+             color: #747d8c"> Made with Italian Sauce,Chicken,anh organice vegetables.</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0;background-color: #ff6b81;
+             ">Order now</button>
+          </div>
+      </div>
+
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./img/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">Food Title</h4>
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">$2,3</h4>
+            <p style="font-size: 1rem;
+             color: #747d8c"> Made with Italian Sauce,Chicken,anh organice vegetables.</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0;background-color: #ff6b81;
+             ">Order now</button>
+          </div>
+      </div>
+  
+    </div>
+
+    <div class="d-flex justify-content-around" style="width: 80%; margin: 50px auto;">
+      
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./anh/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">Food Title</h4>
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">$2,3</h4>
+            <p style="font-size: 1rem;
+             color: #747d8c"> Made with Italian Sauce,Chicken,anh organice vegetables.</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0;background-color: #ff6b81;
+             ">Order now</button>
+          </div>
+      </div>
+
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./anh/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">Food Title</h4>
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">$2,3</h4>
+            <p style="font-size: 1rem;
+             color: #747d8c"> Made with Italian Sauce,Chicken,anh organice vegetables.</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0;background-color: #ff6b81;
+             ">Order now</button>
+          </div>
+      </div>
+  
+    </div>
+
+
+
+    <div class="d-flex justify-content-around" style="width: 80%; margin: 50px auto;">
+      
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./anh/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">Food Title</h4>
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">$2,3</h4>
+            <p style="font-size: 1rem;
+             color: #747d8c"> Made with Italian Sauce,Chicken,anh organice vegetables.</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0;background-color: #ff6b81;
+             ">Order now</button>
+          </div>
+      </div>
+
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./anh/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">Food Title</h4>
+            <h4 style="font-family: Arial, Helvetica, sans-serif;">$2,3</h4>
+            <p style="font-size: 1rem;
+             color: #747d8c"> Made with Italian Sauce,Chicken,anh organice vegetables.</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0;background-color: #ff6b81;
+             ">Order now</button>
+          </div>
+      </div>
+  
+    </div>
+   
+  </div>
+ 
+  <div class="container-fluid d-flex justify-content-center">
+    <div class=" d-flex justify-content-around" style=" width: 40%">
+    <a href="#"><img src="https://img.icons8.com/fluency/48/000000/facebook-circled.png"/></a>
+    <a href="#"><img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/></a>
+    <a href="#"> <img src="https://img.icons8.com/fluency/48/000000/twitter.png"/></a>
+    </div>
+
+
+  </div>
+    <section class="footer">
+        
+        <div class="container text-center">
+              <p>All rights reserved. Designed By <a href="#"  style="color: #ff6b81;">CSE.TLU</a></p>
+        </div>
+        
+      
+     </section>
+
+</body>
+</html>
